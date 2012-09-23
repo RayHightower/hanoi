0ff91edcba827ef6ac1ad3979cd9d2654ee63166
Evan Light <evan@tripledogdare.net>
2012-09-22 23:32:09 -0400

    Corrected wording

--- test/test_hanoi.rb
+++ test/test_hanoi.rb
@@ -9,12 +9,11 @@ describe "Towers of Hanoi" do
       assert_equal state, @hanoi.disks_per_tower
     end
 
-    it "moves one disk at a time" do
+    it "moves one disk at a time, using the upper disk" do
       @hanoi.move(0, 1)
       assert_equal [(1..7).to_a, [8], []], @hanoi.disks_per_tower
     end
 
-    it "moves the upper disk from one tower"
     it "moves the disk atop a larger disk"
     it "cannot move the disk atop a smaller disk"
   end
