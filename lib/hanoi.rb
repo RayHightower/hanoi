5614b832662c46f4a2f93f374a692c4f1a69db1a
Evan Light <evan@tripledogdare.net>
2012-09-22 22:52:11 -0400

    Refactored tests; The board doesnt need to know the number of disks per Tower.. the Towers do.

--- lib/hanoi.rb
+++ lib/hanoi.rb
@@ -1,8 +1,5 @@
 class Hanoi
   class Board
-    def initialize(num_disks)
-    end
-
     def towers
       (1..3).map { Hanoi::Tower.new }
     end
--- test/test_hanoi.rb
+++ test/test_hanoi.rb
@@ -1,21 +1,23 @@
 require 'helper'
 
 describe "Towers of Hanoi" do
+  describe "the game in initial state" do
+    describe "in initial state" do
+      it "has a stack of disks on one tower"
+      it "has two other empty towers"
+    end
+  end
+
   describe "Board" do
     before do
       @num_disks = 8
-      @board = Hanoi::Board.new(@num_disks)
+      @board = Hanoi::Board.new
     end
 
     it "has three towers" do
       assert_equal 3, @board.towers.length
       assert @board.towers.all? { |t| t.instance_of? Hanoi::Tower },
         "All objects in #towers are not Towers"
-    end
-
-    describe "in initial state" do
-      it "has a stack of disks on one tower"
-      it "has two other empty towers"
     end
   end
 
