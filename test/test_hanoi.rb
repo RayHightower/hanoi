c5da03a246fa9750ad93ac05959180a0f23f3871
Evan Light <evan@tripledogdare.net>
2012-09-22 22:39:46 -0400

    Pending specs plus a failing testw

--- test/test_hanoi.rb
+++ test/test_hanoi.rb
@@ -1,7 +1,26 @@
 require 'helper'
 
-class TestHanoi < MiniTest::Unit::TestCase
-  def test_something_for_real
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+describe "Towers of Hanoi" do
+  describe "Board" do
+    before do
+      @num_disks = 8
+      @board = Hanoi::Board.new(@num_disks)
+    end
+
+    it "has three towers" do
+      assert_equal 3, @board.towers.length
+    end
+
+    describe "in initial state" do
+      it "has a stack of disks on one tower"
+      it "has two other empty towers"
+    end
+  end
+
+  describe "when moving a disk" do
+    it "moves one disk at a time"
+    it "moves the upper disk from one tower"
+    it "moves the disk atop a larger disk"
+    it "cannot move the disk atop a smaller disk"
   end
 end
